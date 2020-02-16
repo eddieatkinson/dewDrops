db8e96821699b71eef152f811ee960fc8cf98e0f
Eddie Atkinson <eddiebatkinson@gmail.com>
2020-02-16 14:31:35 -0500

    changed layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,69 +1,74 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
 import { rhythm, scale } from "../utils/typography"
 
-class Layout extends React.Component {
+class Layout extends Component {
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
     const blogPath = `${__PATH_PREFIX__}/blog/`
-    let header
+    let header = (
+      <h1>
+        <Link to={"/"}>{title}</Link>
+      </h1>
+    )
 
-    if (location.pathname === rootPath || location.pathname === blogPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={location.pathname === blogPath ? `/blog/` : `/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+    // if (location.pathname === rootPath || location.pathname === blogPath) {
+    //   header = (
+    //     <h1
+    //       style={{
+    //         ...scale(1.5),
+    //         marginBottom: rhythm(1.5),
+    //         marginTop: 0,
+    //       }}
+    //     >
+    //       <Link
+    //         style={{
+    //           boxShadow: `none`,
+    //           textDecoration: `none`,
+    //           color: `inherit`,
+    //         }}
+    //         to={"/"}
+    //       >
+    //         {title}
+    //       </Link>
+    //     </h1>
+    //   )
+    // } else {
+    //   header = (
+    //     <h3
+    //       style={{
+    //         fontFamily: `Montserrat, sans-serif`,
+    //         marginTop: 0,
+    //       }}
+    //     >
+    //       <Link
+    //         style={{
+    //           boxShadow: `none`,
+    //           textDecoration: `none`,
+    //           color: `inherit`,
+    //         }}
+    //         // to={location.pathname === blogPath ? `/blog/` : `/`}
+    //         to={`/blog/`}
+    //       >
+    //         {title}
+    //       </Link>
+    //     </h3>
+    //   )
+    // }
     return (
       <Wrapper>
+        <Header>{header}</Header>
         <div
           style={{
             marginLeft: `auto`,
             marginRight: `auto`,
             maxWidth: rhythm(24),
-            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            //   padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
-          <header>{header}</header>
           <main>{children}</main>
         </div>
         <Footer>© {new Date().getFullYear()}, Built for Gwenny</Footer>
@@ -71,6 +76,13 @@ class Layout extends React.Component {
     )
   }
 }
+
+const Header = styled.div`
+  padding: 60px;
+  text-align: center;
+  color: white;
+  background: green;
+`
 
 const Wrapper = styled.div`
   min-height: 100vh;
