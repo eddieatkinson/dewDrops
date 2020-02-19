4d4605bf85ab50f552159ea6aa11d941af803979
Eddie Atkinson <eddiebatkinson@gmail.com>
2020-02-18 22:06:38 -0500

    adjusted layout for header and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,71 +2,28 @@ import React, { Component } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
-import { rhythm, scale } from "../utils/typography"
-
 class Layout extends Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    const blogPath = `${__PATH_PREFIX__}/blog/`
-    let header = (
-      <h1>
-        <Link to={"/"}>{title}</Link>
-      </h1>
-    )
+    const { title, children } = this.props
 
-    // if (location.pathname === rootPath || location.pathname === blogPath) {
-    //   header = (
-    //     <h1
-    //       style={{
-    //         ...scale(1.5),
-    //         marginBottom: rhythm(1.5),
-    //         marginTop: 0,
-    //       }}
-    //     >
-    //       <Link
-    //         style={{
-    //           boxShadow: `none`,
-    //           textDecoration: `none`,
-    //           color: `inherit`,
-    //         }}
-    //         to={"/"}
-    //       >
-    //         {title}
-    //       </Link>
-    //     </h1>
-    //   )
-    // } else {
-    //   header = (
-    //     <h3
-    //       style={{
-    //         fontFamily: `Montserrat, sans-serif`,
-    //         marginTop: 0,
-    //       }}
-    //     >
-    //       <Link
-    //         style={{
-    //           boxShadow: `none`,
-    //           textDecoration: `none`,
-    //           color: `inherit`,
-    //         }}
-    //         // to={location.pathname === blogPath ? `/blog/` : `/`}
-    //         to={`/blog/`}
-    //       >
-    //         {title}
-    //       </Link>
-    //     </h3>
-    //   )
-    // }
     return (
       <Wrapper>
-        <Header>{header}</Header>
+        <Header>
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={"/"}
+          >
+            {title}
+          </Link>
+        </Header>
         <div
           style={{
-            marginLeft: `auto`,
-            marginRight: `auto`,
-            maxWidth: rhythm(24),
-            //   padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            margin: "auto",
+            padding: "2rem 10rem",
           }}
         >
           <main>{children}</main>
@@ -78,7 +35,7 @@ class Layout extends Component {
 }
 
 const Header = styled.div`
-  padding: 60px;
+  padding: 3rem;
   text-align: center;
   color: white;
   background: green;
@@ -89,8 +46,9 @@ const Wrapper = styled.div`
 `
 
 const Footer = styled.footer`
+  padding: 3rem;
   text-align: center;
-  margin: 24px;
+  background: #dcdcdc;
 `
 
 export default Layout
